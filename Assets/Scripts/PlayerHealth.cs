47ad383457fe23aac67046ede90647b439eadc66
Dane Camacho <danejcamacho@gmail.com>
2025-01-02 16:21:40 -0800

    Added Heart System

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -4,30 +4,66 @@ public class PlayerHealth : MonoBehaviour
 {
     //Each heart is 2 health points
     public int health = 6;
+    public GameObject[] hearts;
 
-    public void TakeDamage(int damage)
+
+    private void TakeDamage(int damage)
     {
         health -= damage;
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i < health)
+            {
+                hearts[i].SetActive(true);
+            }
+            else
+            {
+                hearts[i].SetActive(false);
+            }
+        }
+
         if (health <= 0)
         {
             Die();
         }
     }
 
+    private void Heal(int damage)
+    {
+        health += damage;
+
+        if (health > 6)
+        {
+            health = 6;
+        }
+
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i < health)
+            {
+                hearts[i].SetActive(true);
+            }
+            else
+            {
+                hearts[i].SetActive(false);
+            }
+        }
+    }
+
+
     void Die()
     {
         //Reload the current scene for now
         UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
     }
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.CompareTag("Enemy"))
+        {
+            TakeDamage(1);
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
